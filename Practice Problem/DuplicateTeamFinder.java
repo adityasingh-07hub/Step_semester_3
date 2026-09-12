4bce2f310ab6e53334778f4a9aa618623410111b
adityasingh-07hub <adityaaingh2007@gmail.com>
2026-09-12 11:24:48 +0530

    Add files via upload

---
+++ Practice Problem/DuplicateTeamFinder.java
@@ -0,0 +1,21 @@
+public class DuplicateTeamFinder {
+    public static void main(String[] args) {
+
+        String[] teams = {"ByteForce", "CodeCrafters", "ByteForce"};
+        String result = findDuplicateTeams(teams);
+        System.out.println(result);
+    }
+
+    private static String findDuplicateTeams(String[] teams) {
+
+        for (int i = 0; i < teams.length; i++) {
+
+            for (int j = i + 1; j < teams.length; j++) {
+                if (teams[i].equals(teams[j])) {
+                    return "Duplicate found : " + teams[i];
+                }
+            }
+        }
+        return "No duplicates found";
+    }
+}
